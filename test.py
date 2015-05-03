ef50b7b12568a6559e572e349a9b17ed5282ced2
Spencer Kaiser <skaiser@me.com>
2015-05-03 15:51:40 -0500

    Adding initial file

---
+++ test.py
@@ -0,0 +1,59 @@
+from Tkinter import Tk, BOTH, RIGHT, RAISED
+from ttk import Frame, Button, Style
+
+#GLOBAL VARIABLES
+
+# ----- Window Variables -----
+width = 600
+height = 700
+
+class Simulator(Frame):
+  
+    def __init__(self, parent):
+        Frame.__init__(self, parent)   
+         
+        self.parent = parent
+        self.pack(fill=BOTH, expand=1)
+        self.centerWindow()
+
+        
+        self.initUI()
+
+    def centerWindow(self):
+      
+        global width
+        global height
+
+        sw = self.parent.winfo_screenwidth()
+        sh = self.parent.winfo_screenheight()
+        
+        x = (sw - width)/2
+        y = (sh - height)/2
+        self.parent.geometry('%dx%d+%d+%d' % (width, height, x, y))
+
+    def initUI(self):
+      
+        self.parent.title("Pipeline Simulator")
+        self.style = Style()
+        self.style.theme_use("default")
+        
+        frame = Frame(self, relief=RAISED, borderwidth=1)
+        frame.pack(fill=BOTH, expand=1)
+
+
+        self.pack(fill=BOTH, expand=1)
+
+        clockButton = Button(self, text="Clock++")
+        clockButton.pack(pady=10)
+
+
+def main():
+  
+    root = Tk()
+    root.geometry("250x150+300+300")
+    app = Simulator(root)
+    root.mainloop()  
+
+
+if __name__ == '__main__':
+    main()  
