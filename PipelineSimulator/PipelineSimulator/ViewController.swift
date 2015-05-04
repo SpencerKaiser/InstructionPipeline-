b397ca3b2d819114fa7efd4c3877b9d19ef03040
Charlie Albright <albrightc25@gmail.com>
2015-05-03 20:19:03 -0500

    Stuff

--- PipelineSimulator/PipelineSimulator/ViewController.swift
+++ PipelineSimulator/PipelineSimulator/ViewController.swift
@@ -25,12 +25,13 @@ class ViewController: UIViewController {
     @IBOutlet weak var advanceClockButton: UIButton!
     
     @IBOutlet weak var instructionLabel: UILabel!
+    @IBOutlet weak var instructionSelector: UISegmentedControl!
     
-    let instructionSet1 = ["LW R8, 4(R3)", "SUB R2, R4, R5", "AND R9, R10, R11", "OR R16, R17, R18", "ADD R13, R14, R0"]
-    let instructionSet2 = ["LW R8, 4(R3)", "SUB R2, R4, R5", "AND R9, R10, R11", "OR R16, R17, R18", "ADD R13, R14, R0"]
-    let instructionSet3 = ["LW R8, 4(R3)", "SUB R2, R4, R5", "AND R9, R10, R11", "OR R16, R17, R18", "ADD R13, R14, R0"]
+    let instructionSet1 = ["ADD R3, R1, R2", "OR R2, R1, R4", "ADD R4, R1, R4", "ADD R5, R3, R4", "AND R6, R4, R5", "OR R8, R6, R7"]
+
 
     var currentInstruction : Int = 0
+    var currentSet : Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -47,11 +48,94 @@ class ViewController: UIViewController {
     }
 
     @IBAction func advanceClock(sender: AnyObject) {
+        
+        if(self.currentSet == 0) {
+            switch self.currentInstruction {
+            case 0:
+                NSLog("CASE 0")
+                self.IF_instr.text = self.instructionSet1[0]
+                break
+            case 1:
+                NSLog("CASE 1")
+                self.IF_instr.text = self.instructionSet1[1]
+                self.ID_instr.text = self.instructionSet1[0]
+                break
+            case 2:
+                NSLog("CASE 2")
+                self.IF_instr.text = self.instructionSet1[2]
+                self.ID_instr.text = self.instructionSet1[1]
+                self.EX_instr.text = self.instructionSet1[0]
+                break
+            case 3:
+                NSLog("CASE 3")
+                self.IF_instr.text = self.instructionSet1[3]
+                self.ID_instr.text = self.instructionSet1[2]
+                self.EX_instr.text = self.instructionSet1[1]
+                self.MEM_instr.text = self.instructionSet1[0]
+                break
+            case 4:
+                NSLog("CASE 4")
+                self.IF_instr.text = self.instructionSet1[4]
+                self.ID_instr.text = self.instructionSet1[3]
+                self.EX_instr.text = self.instructionSet1[2]
+                self.MEM_instr.text = self.instructionSet1[1]
+                self.WB_instr.text = self.instructionSet1[0]
+                break
+            case 5:
+                NSLog("CASE 5")
+                self.IF_instr.text = self.instructionSet1[5]
+                self.ID_instr.text = self.instructionSet1[4]
+                self.MEM_instr.text = self.instructionSet1[2]
+                self.WB_instr.text = self.instructionSet1[1]
+                self.EX_bg.backgroundColor = UIColor.redColor()
+                self.EX_instr.text = "STALL"
+                break
+            case 6:
+                NSLog("CASE 6")
+                break
+            case 7:
+                NSLog("CASE 7")
+                break
+            case 8:
+                NSLog("CASE 8")
+                break
+            case 9:
+                NSLog("CASE 9")
+                break
+            case 10:
+                NSLog("CASE 10")
+                break
+            case 11:
+                NSLog("CASE 11")
+                break
+            case 12:
+                NSLog("CASE 12")
+                break
+            case 13:
+                NSLog("CASE 13")
+                break
+            default:
+                NSLog("CASE DEFAULT")
+                self.currentInstruction = -1
+                break
+            }
+        } else if(self.currentSet == 1) {
+            switch self.currentInstruction {
+                default:
+                    break
+            }
+        } else {
+            switch self.currentInstruction {
+                default:
+                    break
+            }
+        }
+        self.currentInstruction++
     }
     
     @IBAction func instructionSetChanged(sender: AnyObject) {
+        self.currentInstruction = 0
+        self.currentSet = self.instructionSelector.selectedSegmentIndex
     }
-
-
 }
 
